7745b27f1abaad0d97d9414a54249b9d38f4049a
Daniel Lüdecke <d.luedecke@uke.de>
2022-08-10 10:50:01 +0000

    version 0.9.2

--- R/check_outliers.R
+++ R/check_outliers.R
@@ -335,7 +335,6 @@ check_outliers <- function(x, ...) {
 #' @rdname check_outliers
 #' @export
 check_outliers.default <- function(x, method = c("cook", "pareto"), threshold = NULL, ...) {
-
   # Check args
   if (all(method == "all")) {
     method <- c(
@@ -476,7 +475,6 @@ check_outliers.numeric <- function(x, method = "zscore_robust", threshold = NULL
 #' @rdname check_outliers
 #' @export
 check_outliers.data.frame <- function(x, method = "mahalanobis", threshold = NULL, ...) {
-
   # Remove non-numerics
   x <- x[, sapply(x, is.numeric), drop = FALSE]
 
@@ -664,6 +662,10 @@ check_outliers.lme <- check_outliers.gls
 
 #' @export
 check_outliers.fixest <- check_outliers.gls
+
+#' @export
+check_outliers.geeglm <- check_outliers.gls
+
 
 
 
